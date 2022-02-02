4f63879959cc252e6ebafab5df34aee276317521
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2022-02-02 18:12:33 -0300

    Questao 05

--- main.py
+++ main.py
@@ -31,3 +31,20 @@ print(tv1.canal)
 tv1.muda_canal_para_cima()
 print(tv1.canal)
 
+#Questão 5
+
+tv3 = Televisao(1, 10)
+tv3.tamanho = 32
+tv3.marca = "Sansung"
+
+print("A televisao tem {} polegadas e eh a marcar {} e atualmente esta no canal {}".format(tv3.tamanho, tv3.marca, tv3.canal))
+
+tv4 = Televisao(1, 10)
+tv4.tamanho = 50
+tv4.marca = "Positivo"
+
+print("A televisao tem {} polegadas e eh a marcar {} e atualmente esta no canal {}".format(tv4.tamanho, tv4.marca, tv4.canal))
+tv4.muda_canal_para_baixo()
+print("O canal foi alterado {}".format(tv4.canal))
+tv4.muda_canal_para_baixo()
+print("O canal foi alterado para {}".format(tv4.canal))
