3ea71c2e43f1064aac0364955bc6b1aa728f32e7
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2022-02-02 17:55:49 -0300

    Mudanças na classe para questão 03

--- Televisao.py
+++ Televisao.py
@@ -1,5 +1,20 @@
 class Televisao():
-    def __init__(self):
+    def __init__(self, min, max):
         self.tamanho = 0
         self.marca = ""
-        self.canal = 2
+        self.canal = min
+        self.cMinino = min
+        self.cMaximo = max
+
+
+    def muda_canal_para_baixo(self):
+        if self.canal-1 >= self.cMinino:
+            self.canal -= 1
+        else:
+            self.canal = self.cMaximo
+
+    def muda_canal_para_cima(self):
+        if self.canal+1 <= self.cMaximo:
+            self.canal += 1
+        else:
+            self.canal = self.cMinino
