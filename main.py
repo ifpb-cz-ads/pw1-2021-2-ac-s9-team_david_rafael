eda792fba2b67c3ab26e7d5911160e4c31ca55fc
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2022-02-02 15:46:05 -0300

    Main com a questao 01

---
+++ main.py
@@ -0,0 +1,12 @@
+from Televisao import Televisao
+
+tv1 = Televisao()
+tv1.tamanho = 10
+tv1.marca = "teste1"
+
+tv2 = Televisao()
+tv2.tamanho = 5
+tv2.marca = "teste2"
+
+print("A tv tem {} Polegadas e eh de {} marca".format(tv1.tamanho,tv1.marca))
+print("A tv tem {} Polegadas e eh de {} marca".format(tv2.tamanho, tv2.marca))
