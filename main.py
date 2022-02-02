490db23b4ab67bd83a4e32b145ce0f473d72621f
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2022-02-02 15:51:58 -0300

    Questao 02

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Televisao import Televisao
 
+
+#Questao 1
 tv1 = Televisao()
 tv1.tamanho = 10
 tv1.marca = "teste1"
@@ -10,3 +12,9 @@ tv2.marca = "teste2"
 
 print("A tv tem {} Polegadas e eh de {} marca".format(tv1.tamanho,tv1.marca))
 print("A tv tem {} Polegadas e eh de {} marca".format(tv2.tamanho, tv2.marca))
+
+
+#Questao 2
+print(tv1.canal)
+tv1.canal = 3
+print(tv1.canal)
